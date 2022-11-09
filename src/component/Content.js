fb0aee68bd26aee2b5a75fdab51be7a0e3937882
abhishekshukla <abhishekshukla2022jan@cedcoss.com>
2022-11-09 19:12:02 +0530

    home ready

--- src/component/Content.js
+++ src/component/Content.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 // import { useEffect } from 'react';
 
-const [mostPopular, setMostPopular] = ({})
 function Content() {
+    const [mostPopular, setMostPopular] = useState({});
     function handleResponce() {
 
         // e.preventDefault();
@@ -11,11 +11,11 @@ function Content() {
             method: 'GET',
         };
         let url = localStorage.getItem('url');
-        fetch(url, requestOptions).then(response => response.json()).then(response => {
-            if (response) {
+        fetch("https://www.googleapis.com/youtube/v3/search?key=AIzaSyBloaji-5YAewRiumRNRt6gpZIZrry5VbY&maxResults=10&part=snippet&type=video&q=143", requestOptions).then(response => response.json()).then(response => {
+            if (Object.keys(response).length > 0) {
 
                 console.log('aya', response);
-                setMostPopular(response);
+                setMostPopular({ ...response });
                 // setTest(response['data']);
                 // setColData(response['data'])
 
@@ -27,25 +27,35 @@ function Content() {
     useEffect(() => {
         handleResponce()
     }, []);
+
+    useEffect(() => {
+        console.log('data1', mostPopular.items)
+    }, [mostPopular]);
     return (
         <div>
             <div className="w3-container w3-teal">
                 <h1>My Page</h1>
             </div>
             <div className='row'>
-                <div className="card" style={{ width: "18rem" }}>
-                    <img className="card-img-top" src="..." alt="Card image cap" />
-                    <div className="card-body">
-                        <h5 className="card-title">{mostPopular.items}</h5>
-                        <p className="card-text">
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </p>
-                        <a href="#" className="btn btn-primary">
-                            Go somewhere
-                        </a>
+                {
+                ( mostPopular?.items?.length>0&& mostPopular.items).map((data) => {
+                    console.log('abc1',data)
+                   return <div className="card" style={{ width: "18rem" }}>
+                        <img className="card-img-top" src={data?.snippet?.thumbnails?.medium?.url} alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">
+                            {data?.snippet?.thumbnails?.title} 
+                                
+                            </h5>
+                            <p className="card-text">
+                            {data?.snippet?.description }.
+                            </p>
+                            <a href="#" className="btn btn-primary">
+                                Go somewhere
+                            </a>
+                        </div>
                     </div>
-                </div>
+                })}
 
             </div>
             <button onClick={handleResponce}>content</button>
