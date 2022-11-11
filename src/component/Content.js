aa6176e9dd09df5e75ff8aadbfbb5630a4196f8f
abhishekshukla <abhishekshukla2022jan@cedcoss.com>
2022-11-11 19:05:14 +0530

    strt

--- src/component/Content.js
+++ src/component/Content.js
@@ -2,24 +2,44 @@ import React, { useEffect, useState } from 'react'
 import IframeContent from './IframeContent';
 
 // import { useEffect } from 'react';
+var i =0
 
 let x = Math.floor(Math.random() * 999);
 
-function Content(search) {
+function Content(props) {
+    console.log("props",props.search);
     const [mostPopular, setMostPopular] = useState({});
+    const [urt, setUrt] = useState("145");
     const [videoId, setVideoId] = useState();
-    console.log("search",search)
+    // console.log("search",search)
+  
     function handleResponce() {
 
         // e.preventDefault();
+        // let url = "";
+        console.log("hann",props.search)
         // const newrecord = { ...MultiInput, id: new Date().getTime().toString() };
         var requestOptions = {
             method: 'GET',
         };
+        // if(props.search !== undefined){
+        //     let sea = props.search;
+        //     console.log("dddd",sea);
+        //     url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCntCSP6aZeGrmPDAaxG0FDr4npIeOHpAg&maxResults=10&part=snippet&type=video&q="+sea;
+        //     console.log("utl",url)
+        // }else{
+
+        //     url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCntCSP6aZeGrmPDAaxG0FDr4npIeOHpAg&maxResults=10&part=snippet&type=video&q="+x;
+        // }
+        let url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyBbWCubWQ6bnDzYYU9tj4Nk-Fdtn0LOaTk&maxResults=10&part=snippet&type=video&q="+urt;
+
+
+        // }
         let url2 = localStorage.getItem('url');
         // AIzaSyBloaji-5YAewRiumRNRt6gpZIZrry5VbY
-        let url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCHi1IRNAtudLEHveNEwFvIGiq6QHj739o&maxResults=10&part=snippet&type=video&q="+x;
-        console.log(url);
+        // consolelog(url);
+        console.log("urllllll=>",url);
+
         fetch(url, requestOptions).then(response => response.json()).then(response => {
             if (Object.keys(response).length > 0) {
 
@@ -34,6 +54,12 @@ function Content(search) {
         }).catch(err => console.error(err));
 
     }
+    function Gethandle() {
+
+        useEffect(() => {
+            handleResponce()
+        }, []);
+    }
     useEffect(() => {
         handleResponce()
     }, []);
@@ -43,42 +69,50 @@ function Content(search) {
     useEffect(() => {
         console.log('data1', mostPopular.items)
     }, [mostPopular]);
+    if(props.search !== undefined){
+        // handleResponce()
+        // Gethandle()
+        setUrt(props.search);
+        i +=1;
+        console.log("iiii",i)
+        
+    }
     return (<>
 
-        <div className="w3-container w3-teal">
-            <h1>My Page</h1>
-        </div>
-        <div>
-            <IframeContent video={videoId} />
-        </div>
-        <div>
-
-            <div className='row'>
-                {
-                    mostPopular?.items?.length > 0 && mostPopular.items.map((data) => {
-                        console.log('abc1', data)
-                        return <div className="card" style={{ width: "18rem" }}>
-                            <img className="card-img-top" src={data?.snippet?.thumbnails?.medium?.url} alt="Card image cap" />
-                            <div className="card-body">
-                                <h5 className="card-title">
-                                    {data?.snippet?.thumbnails?.title}
-
-                                </h5>
-                                <p className="card-text">
-                                    {data?.snippet?.description}.
-                                </p>
-                                <button onClick={(()=>{
-                                    sendId(data.id.videoId)
-                                })}>
-                                    Watch Now
-                                </button>
-                            </div>
-                        </div>
-                    })}
-
+            <div className="w3-container w3-teal">
+                <h1>My Page</h1>
             </div>
-            <button onClick={handleResponce}>content</button>
-        </div>
+            <div>
+                <IframeContent video={videoId} />
+            </div>
+            <div>
+
+                <div className='row'>
+                    {
+                        mostPopular?.items?.length > 0 && mostPopular.items.map((data) => {
+                            console.log('abc1', data)
+                            return <div className="card" style={{ width: "18rem" }}>
+                                <img className="card-img-top" src={data?.snippet?.thumbnails?.medium?.url} alt="Card image cap" />
+                                <div className="card-body">
+                                    <h5 className="card-title">
+                                        {data?.snippet?.thumbnails?.title}
+
+                                    </h5>
+                                    <p className="card-text">
+                                        {data?.snippet?.description}.
+                                    </p>
+                                    <button onClick={(()=>{
+                                        sendId(data.id.videoId)
+                                    })}>
+                                        Watch Now
+                                    </button>
+                                </div>
+                            </div>
+                        })}
+
+                </div>
+                <button onClick={handleResponce}>content</button>
+            </div>
     </>
     )
 }
